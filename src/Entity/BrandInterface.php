5f953d061fb77d5671cac948421f8b167f7d75e6
RiKaZaRai <41432142+RiKaZaRai@users.noreply.github.com>
2025-09-02 16:58:09 +0000

    v1.1.13

--- src/Entity/BrandInterface.php
+++ src/Entity/BrandInterface.php
@@ -4,166 +4,46 @@ declare(strict_types=1);
 
 namespace Rika\SyliusBrandPlugin\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Sylius\Component\Product\Model\ProductInterface;
-use Sylius\Resource\Model\ResourceInterface;
-use Sylius\Resource\Model\SlugAwareInterface;
-use Sylius\Resource\Model\TimestampableTrait;
-use Sylius\Resource\Model\ToggleableTrait;
-use Sylius\Resource\Model\TranslatableTrait;
+use Sylius\Resource\Model\ToggleableInterface;
+use Sylius\Resource\Model\TimestampableInterface;
 use Sylius\Resource\Model\TranslationInterface;
 
-class Brand implements BrandInterface
+interface BrandInterface extends ToggleableInterface, TimestampableInterface
 {
-    use TimestampableTrait;
-    use ToggleableTrait;
-    use TranslatableTrait {
-        __construct as private initializeTranslationsCollection;
-        getTranslation as private doGetTranslation;
-    }
+    public function getId(): ?int;
 
-    protected ?int $id = null;
-    protected ?string $code = null;
-    protected ?string $logoPath = null;
-    protected ?int $position = null;
-    protected Collection $products;
+    public function getCode(): ?string;
+    public function setCode(?string $code): void;
 
-    public function __construct()
-    {
-        $this->initializeTranslationsCollection();
-        $this->products = new ArrayCollection();
-        $this->createdAt = new \DateTime();
-    }
+    public function getLogoPath(): ?string;
+    public function setLogoPath(?string $logoPath): void;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getCode(): ?string
-    {
-        return $this->code;
-    }
-
-    public function setCode(?string $code): void
-    {
-        $this->code = $code;
-    }
-
-    public function getLogoPath(): ?string
-    {
-        return $this->logoPath;
-    }
-
-    public function setLogoPath(?string $logoPath): void
-    {
-        $this->logoPath = $logoPath;
-    }
-
-    public function getPosition(): ?int
-    {
-        return $this->position;
-    }
-
-    public function setPosition(?int $position): void
-    {
-        $this->position = $position;
-    }
-
-    public function getProducts(): Collection
-    {
-        return $this->products;
-    }
-
-    public function addProduct(ProductInterface $product): void
-    {
-        if (!$this->hasProduct($product)) {
-            $this->products->add($product);
-            $product->setBrand($this);
-        }
-    }
-
-    public function removeProduct(ProductInterface $product): void
-    {
-        if ($this->hasProduct($product)) {
-            $this->products->removeElement($product);
-            $product->setBrand(null);
-        }
-    }
-
-    public function hasProduct(ProductInterface $product): bool
-    {
-        return $this->products->contains($product);
-    }
-
-    // Méthodes de délégation pour les traductions
-    public function getName(): ?string
-    {
-        return $this->getTranslation()->getName();
-    }
-
-    public function setName(?string $name): void
-    {
-        $this->getTranslation()->setName($name);
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->getTranslation()->getDescription();
-    }
-
-    public function setDescription(?string $description): void
-    {
-        $this->getTranslation()->setDescription($description);
-    }
-
-    public function getMetaKeywords(): ?string
-    {
-        return $this->getTranslation()->getMetaKeywords();
-    }
-
-    public function setMetaKeywords(?string $metaKeywords): void
-    {
-        $this->getTranslation()->setMetaKeywords($metaKeywords);
-    }
-
-    public function getMetaDescription(): ?string
-    {
-        return $this->getTranslation()->getMetaDescription();
-    }
-
-    public function setMetaDescription(?string $metaDescription): void
-    {
-        $this->getTranslation()->setMetaDescription($metaDescription);
-    }
-
-    public function setEnabled(?bool $enabled): void
-    {
-        $this->enabled = $enabled ?? false;
-    }
+    public function getPosition(): ?int;
+    public function setPosition(?int $position): void;
 
     /**
-     * {@inheritdoc}
+     * @return ProductInterface[]
      */
-    public function getTranslation(?string $locale = null): TranslationInterface
-    {
-        /** @var BrandTranslationInterface $translation */
-        $translation = $this->doGetTranslation($locale);
+    public function getProducts(): iterable;
+    public function addProduct(ProductInterface $product): void;
+    public function removeProduct(ProductInterface $product): void;
+    public function hasProduct(ProductInterface $product): bool;
 
-        return $translation;
-    }
+    public function getName(): ?string;
+    public function setName(?string $name): void;
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function createTranslation(): TranslationInterface
-    {
-        return new BrandTranslation();
-    }
+    public function getDescription(): ?string;
+    public function setDescription(?string $description): void;
 
-    public function __toString(): string
-    {
-        return (string) $this->getName();
-    }
+    public function getMetaKeywords(): ?string;
+    public function setMetaKeywords(?string $metaKeywords): void;
+
+    public function getMetaDescription(): ?string;
+    public function setMetaDescription(?string $metaDescription): void;
+
+    public function getSlug(): ?string;
+    public function setSlug(?string $slug): void;
+
+    public function getTranslation(?string $locale = null): TranslationInterface;
 }
