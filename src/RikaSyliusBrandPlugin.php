27895b9b4d4cedef262c831cbf95d6a3711c9f23
RiKaZaRai <41432142+RiKaZaRai@users.noreply.github.com>
2025-09-02 11:27:56 +0000

    Fix: Remove invalid fields option and add grid configuration - v1.0.32

--- src/RikaSyliusBrandPlugin.php
+++ src/RikaSyliusBrandPlugin.php
@@ -6,8 +6,15 @@ namespace Rika\SyliusBrandPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Rika\SyliusBrandPlugin\DependencyInjection\RikaSyliusBrandExtension;
 
 final class RikaSyliusBrandPlugin extends Bundle
 {
     use SyliusPluginTrait;
+
+    public function getContainerExtension(): RikaSyliusBrandExtension
+    {
+        return new RikaSyliusBrandExtension();
+    }
 }
