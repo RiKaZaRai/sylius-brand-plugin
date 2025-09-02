5993a2e4e7730aa9adb0e15728196e0904f845d1
RiKaZaRai <41432142+RiKaZaRai@users.noreply.github.com>
2025-09-02 10:00:59 +0000

    Fix resource configuration for Sylius 2.1 compatibility

--- src/DependencyInjection/RikaSyliusBrandExtension.php
+++ src/DependencyInjection/RikaSyliusBrandExtension.php
@@ -5,36 +5,45 @@ declare(strict_types=1);
 
 namespace Rika\SyliusBrandPlugin\DependencyInjection;
 
-use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-final class RikaSyliusBrandExtension extends Extension implements PrependExtensionInterface
+final class RikaSyliusBrandExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        // Traiter la configuration si elle existe
-        $configuration = $this->getConfiguration([], $container);
-        if ($configuration instanceof ConfigurationInterface) {
-            $this->processConfiguration($configuration, $configs);
-        }
-
-        // Charger uniquement les fichiers YAML existants dans Resources/config
+        $config = $this->processConfiguration($this->getConfiguration([], $container), $configs);
+        
+        // Charger les services
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $yamlFiles = ['services.yaml', 'sylius_resource.yaml']; // liste explicite
-        foreach ($yamlFiles as $file) {
-            if (file_exists(__DIR__ . '/../Resources/config/' . $file)) {
-                $loader->load($file);
-            }
-        }
+        $loader->load('services.yaml');
+
+        // Configuration des ressources directement dans l'extension
+        $this->registerResources('rika_sylius_brand', 'doctrine/orm', [
+            'brand' => [
+                'classes' => [
+                    'model' => 'Rika\SyliusBrandPlugin\Entity\Brand',
+                    'interface' => 'Rika\SyliusBrandPlugin\Entity\BrandInterface',
+                    'repository' => 'Rika\SyliusBrandPlugin\Repository\BrandRepository',
+                    'form' => 'Rika\SyliusBrandPlugin\Form\Type\BrandType',
+                ],
+                'translation' => [
+                    'classes' => [
+                        'model' => 'Rika\SyliusBrandPlugin\Entity\BrandTranslation',
+                        'interface' => 'Rika\SyliusBrandPlugin\Entity\BrandTranslationInterface',
+                        'form' => 'Rika\SyliusBrandPlugin\Form\Type\BrandTranslationType',
+                    ],
+                ],
+            ],
+        ], $container);
     }
 
     public function prepend(ContainerBuilder $container): void
     {
-        // Configurer Doctrine pour les mappings XML de ton plugin
+        // Configurer Doctrine pour les mappings XML
         if ($container->hasExtension('doctrine')) {
             $container->prependExtensionConfig('doctrine', [
                 'orm' => [
