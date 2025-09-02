f0adbba30658060c881e0c78faeded9430f02ead
RiKaZaRai <41432142+RiKaZaRai@users.noreply.github.com>
2025-09-02 14:08:13 +0000

    v1.0.60

--- src/Form/Type/BrandType.php
+++ src/Form/Type/BrandType.php
@@ -6,11 +6,11 @@ namespace Rika\SyliusBrandPlugin\Form\Type;
 
 use Rika\SyliusBrandPlugin\Entity\Brand;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
-use Sylius\Bundle\ResourceBundle\Form\Type\ResourceTranslationsType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 final class BrandType extends AbstractResourceType
@@ -22,14 +22,16 @@ final class BrandType extends AbstractResourceType
                 'label' => 'rika_sylius_brand.form.brand.code',
                 'required' => true,
             ])
-            ->add('translations', ResourceTranslationsType::class, [
-                'entry_type' => BrandTranslationType::class,
-                'label' => 'rika_sylius_brand.form.brand.translations',
-            ])
-            ->add('logoPath', FileType::class, [
-                'label' => 'rika_sylius_brand.form.brand.logo',
-                'required' => false,
+            // Suppression temporaire des traductions
+            ->add('name', TextType::class, [
+                'label' => 'rika_sylius_brand.form.brand.name',
                 'mapped' => false,
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'rika_sylius_brand.form.brand.description',
+                'mapped' => false,
+                'required' => false,
             ])
             ->add('enabled', CheckboxType::class, [
                 'label' => 'rika_sylius_brand.form.brand.enabled',
