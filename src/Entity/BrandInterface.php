ccfe55a51e87dc2d5df238c2497296fc68ba92ff
RiKaZaRai <41432142+RiKaZaRai@users.noreply.github.com>
2025-09-03 07:46:20 +0000

    v1.1.29

--- src/Entity/BrandInterface.php
+++ src/Entity/BrandInterface.php
@@ -4,46 +4,61 @@ declare(strict_types=1);
 
 namespace Rika\SyliusBrandPlugin\Entity;
 
+use Doctrine\Common\Collections\Collection;
 use Sylius\Component\Product\Model\ProductInterface;
-use Sylius\Resource\Model\ToggleableInterface;
+use Sylius\Resource\Model\ResourceInterface;
+use Sylius\Resource\Model\SlugAwareInterface;
 use Sylius\Resource\Model\TimestampableInterface;
-use Sylius\Resource\Model\TranslationInterface;
+use Sylius\Resource\Model\ToggleableInterface;
+use Sylius\Resource\Model\TranslatableInterface;
 
-interface BrandInterface extends ToggleableInterface, TimestampableInterface
+interface BrandInterface extends 
+    ResourceInterface, 
+    SlugAwareInterface, 
+    TimestampableInterface, 
+    ToggleableInterface,
+    TranslatableInterface
 {
-    public function getId(): ?int;
-
     public function getCode(): ?string;
+
     public function setCode(?string $code): void;
 
-    public function getLogoPath(): ?string;
-    public function setLogoPath(?string $logoPath): void;
-
-    public function getPosition(): ?int;
-    public function setPosition(?int $position): void;
-
-    /**
-     * @return ProductInterface[]
-     */
-    public function getProducts(): iterable;
-    public function addProduct(ProductInterface $product): void;
-    public function removeProduct(ProductInterface $product): void;
-    public function hasProduct(ProductInterface $product): bool;
-
     public function getName(): ?string;
+
     public function setName(?string $name): void;
 
+    public function getSlug(): ?string;
+
+    public function setSlug(?string $slug): void;
+
     public function getDescription(): ?string;
+
     public function setDescription(?string $description): void;
 
     public function getMetaKeywords(): ?string;
+
     public function setMetaKeywords(?string $metaKeywords): void;
 
     public function getMetaDescription(): ?string;
+
     public function setMetaDescription(?string $metaDescription): void;
 
-    public function getSlug(): ?string;
-    public function setSlug(?string $slug): void;
+    public function getLogoPath(): ?string;
 
-    public function getTranslation(?string $locale = null): TranslationInterface;
+    public function setLogoPath(?string $logoPath): void;
+
+    public function getPosition(): ?int;
+
+    public function setPosition(?int $position): void;
+
+    /**
+     * @return Collection<int, ProductInterface>
+     */
+    public function getProducts(): Collection;
+
+    public function hasProduct(ProductInterface $product): bool;
+
+    public function addProduct(ProductInterface $product): void;
+
+    public function removeProduct(ProductInterface $product): void;
 }
