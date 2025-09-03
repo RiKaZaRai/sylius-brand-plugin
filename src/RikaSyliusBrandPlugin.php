4b07474c0b5e1afbe682ca18de070a8040a029a5
RiKaZaRai <41432142+RiKaZaRai@users.noreply.github.com>
2025-09-03 16:49:17 +0000

    Move translations directory to root following Symfony 7.3 conventions

--- src/RikaSyliusBrandPlugin.php
+++ src/RikaSyliusBrandPlugin.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Rika\SyliusBrandPlugin;
 
+use Rika\SyliusBrandPlugin\DependencyInjection\RikaSyliusBrandExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
@@ -15,9 +17,18 @@ final class RikaSyliusBrandPlugin extends AbstractBundle
         parent::build($container);
     }
 
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new RikaSyliusBrandExtension();
+    }
+
     public function configureRoutes(RoutingConfigurator $routes, ?string $environment): void
     {
-        // Importe automatiquement toutes les routes définies dans ton plugin
         $routes->import($this->getPath() . '/Resources/config/routes.yaml');
+    }
+
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
     }
 }
