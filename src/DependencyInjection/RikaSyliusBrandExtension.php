abf7a7737645ce04c8a5cb2b7c1716e9a7bf8bf6
RiKaZaRai <41432142+RiKaZaRai@users.noreply.github.com>
2025-09-05 09:43:55 +0000

    v2.0.29

--- src/DependencyInjection/RikaSyliusBrandExtension.php
+++ src/DependencyInjection/RikaSyliusBrandExtension.php
@@ -7,10 +7,8 @@ namespace Rika\SyliusBrandPlugin\DependencyInjection;
 use Sylius\Bundle\CoreBundle\DependencyInjection\PrependDoctrineMigrationsTrait;
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 final class RikaSyliusBrandExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
@@ -18,11 +16,7 @@ final class RikaSyliusBrandExtension extends AbstractResourceExtension implement
 
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
-        $loader->load('services.yaml');
-
-        /** @var ConfigurationInterface $configuration */
-        $configuration = $this->getConfiguration([], $container);
+        $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         
         $container->setParameter('rika_sylius_brand.upload_dir', '%kernel.project_dir%/public/media/brands');
