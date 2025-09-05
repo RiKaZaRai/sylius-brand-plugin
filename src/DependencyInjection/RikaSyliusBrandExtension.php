9f62264f32db7effc38069a200e3395abab025b7
RiKaZaRai <41432142+RiKaZaRai@users.noreply.github.com>
2025-09-05 15:47:48 +0000

    v2.0.45

--- src/DependencyInjection/RikaSyliusBrandExtension.php
+++ src/DependencyInjection/RikaSyliusBrandExtension.php
@@ -19,7 +19,7 @@ final class RikaSyliusBrandExtension extends AbstractResourceExtension implement
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
-        $loader->load('services.yaml');
+        $loader->load('config.yaml');
     }
 
     public function prepend(ContainerBuilder $container): void
